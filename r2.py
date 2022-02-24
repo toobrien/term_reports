2183cde6569b0b1ea94b3226a52b343a2bbffa8e
Taylor O'Brien <toobrien@amazon.com>
2022-02-23 17:54:44 -0800

    added implied discount rate

--- r2.py
+++ r2.py
@@ -1,16 +1,17 @@
 from    bisect  import bisect_left
 from    json    import loads
+from    math    import log
 from    sys     import argv
 from    time    import time
 from    typing  import List
-from    util    import clean, get_groups, r, spreads
+from    util    import clean, get_groups, r, rs, spreads
 
 config  = loads(open("./config.json").read())
 
 DB_PATH     = config["db_path"]
 START       = config["start"]
 END         = config["end"]
-MAX_MONTHS  = 12
+MAX_MONTHS  = 48
 
 
 def report(groups: List):
@@ -65,50 +66,55 @@ def report(groups: List):
     discount_rank   = bisect_left(discounts, latest_discount) / len(discounts)
 
     print(
-        "date".ljust(12),
-        "symbol".ljust(12),
-        "spot".ljust(12),
-        "avg. discount".ljust(12),
-        "discount rank".ljust(12)
+        "date".rjust(12),
+        "symbol".rjust(12),
+        "spot".rjust(12),
+        "avg. discount".rjust(12),
+        "discount rank".rjust(12)
     )
 
     print(
-        f"{latest[0][r.date]}".ljust(12),
-        f"{latest[0][r.name]}".ljust(12),
-        f"{latest[0][r.spot]: 0.3f}".ljust(12),
-        f"{latest_discount: 0.3f}".ljust(12),
-        f"{discount_rank: 0.3f}".ljust(12),
+        f"{latest[0][r.date]}".rjust(12),
+        f"{latest[0][r.name]}".rjust(12),
+        f"{latest[0][r.spot]: 0.3f}".rjust(12),
+        f"{latest_discount: 0.3f}".rjust(12),
+        f"{discount_rank: 0.3f}".rjust(12),
         "\n"
     )
 
     # spread report
 
     print(
-        "id".ljust(12), 
-        "size pct.".ljust(12),
-        "size pt.".ljust(12),
-        "rank (all)".ljust(12), 
-        "rank (season)".ljust(12)
+        "id".rjust(12), 
+        "% spot".rjust(12),
+        "impl. rate".rjust(12),
+        "size ".rjust(12),
+        "rank (all)".rjust(12), 
+        "rank (season)".rjust(12)
     )
 
     for spread in latest:
 
-        id          = f"{spread[r.id][0]}/{spread[r.id][1]}"
-        size_pct    = spread[r.settle] / spread[r.spot]
-        size_pt     = spread[r.settle]
-        rank_all    = bisect_left(size_all, size_pct) / len(size_all)
-        size_season = sorted([
-            row[r.settle] / row[r.spot]
-            for row in by_season[spread[r.month]]
-        ])
-        rank_season = bisect_left(size_season, size_pct) / len(size_season)
+        id          = f"{spread[rs.id][0]}/{spread[rs.id][1]}"
+        pct_spot    = spread[rs.settle] / spread[rs.spot]
+        impl_rate   = log((spread[rs.spot] - spread[rs.settle]) / spread[rs.spot]) / ((spread[rs.dte_back] - spread[rs.dte]) / 365)
+        point       = spread[rs.settle]
+        rank_all    = bisect_left(size_all, pct_spot) / len(size_all)
+        size_season = sorted(
+            [
+                spread_row[rs.settle] / spread_row[rs.spot]
+                for spread_row in by_season[spread[rs.month]]
+            ]
+        )
+        rank_season = bisect_left(size_season, pct_spot) / len(size_season)
 
         print(
-            f"{id: <12}",
-            f"{size_pct: <12.3f}",
-            f"{size_pt: <12.3f}"
-            f"{rank_all: <12.3f}",
-            f"{rank_season: <12.3f}",
+            f"{id: >12}",
+            f"{pct_spot: >12.3f}",
+            f"{impl_rate: >12.3f}",
+            f"{point: >12.3f}"
+            f"{rank_all: >12.3f}",
+            f"{rank_season: >12.3f}",
         )
 
 
--- util.py
+++ util.py
@@ -20,6 +20,17 @@ class r(IntEnum):
     spot        = 6
     dte         = 7
 
+class rs(IntEnum):
+
+    id          = 0
+    date        = 1
+    name        = 2
+    month       = 3
+    year        = 4
+    settle      = 5
+    spot        = 6
+    dte         = 7
+    dte_back    = 8
 
 def get_groups(
     symbol: str, 
@@ -161,19 +172,20 @@ def spreads(groups: List, width: int):
 
         for j in range(len(group) - width):
 
-            spread_record = [ None ] * len(r)
+            spread_record = [ None ] * len(rs)
 
-            spread_record[r.id]     = ( 
-                f"{group[j][r.month]}{group[j][r.id][-2:]}",
-                f"{group[j + width][r.month]}{group[j + width][r.id][-2:]}",
+            spread_record[rs.id]     = ( 
+                f"{group[j][rs.month]}{group[j][rs.id][-2:]}",
+                f"{group[j + width][rs.month]}{group[j + width][rs.id][-2:]}",
             )
-            spread_record[r.date]   = group[j][r.date]
-            spread_record[r.dte]    = group[j][r.dte]
-            spread_record[r.name]   = group[j][r.name]
-            spread_record[r.month]  = (group[j][r.month], group[j + width][r.month])
-            spread_record[r.year]   = (group[j][r.year], group[j + 1][r.year])
-            spread_record[r.settle] = group[j + width][r.settle] - group[j][r.settle]
-            spread_record[r.spot]   = group[j][r.spot]
+            spread_record[rs.date]      = group[j][r.date]
+            spread_record[rs.dte]       = group[j][r.dte]
+            spread_record[rs.dte_back]  = group[j + width][r.dte]
+            spread_record[rs.name]      = group[j][r.name]
+            spread_record[rs.month]     = (group[j][r.month], group[j + width][r.month])
+            spread_record[rs.year]      = (group[j][r.year], group[j + 1][r.year])
+            spread_record[rs.settle]    = group[j + width][r.settle] - group[j][r.settle]
+            spread_record[rs.spot]      = group[j][r.spot]
 
             spread_group.append(spread_record)
 
