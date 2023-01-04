5fd24d7d796feb936f3ae38ba8cdd1e8d1e92c09
Taylor O'Brien <toobrien@amazon.com>
2023-01-03 23:22:03 -0800

    added outright mode to term_chg_2d

--- term_chg_2d.py
+++ term_chg_2d.py
@@ -16,57 +16,98 @@ def report(
     max_terms:  int
 ):
 
-    groups  = get_groups(symbol, START, END, False)[-days:]
-    spreads = {}
+    groups      = get_groups(symbol, START, END, False)[-days:]
+    spreads     = {}
+    outrights   = {}
 
-    for i in range(1, len(groups)):
+    # outrights
 
-        yesterday   = groups[i - 1]
-        today       = groups[i]
+    for group in groups:
 
-        date = today[0][r.date]
+        for rec in group[:max_terms]:
 
-        for j in range(spread_len, min(len(yesterday), len(today), max_terms), spread_len):
+            id = rec[r.id].split("_")[1][-5:]
+            id = id[0] + id[-2:]
 
-            m1_idx = j - spread_len
-            m2_idx = j
+            if id not in outrights:
 
-            m1_rec_today        = today[m1_idx]
-            m2_rec_today        = today[m2_idx]
-            m1_rec_yesterday    = yesterday[m1_idx]
-            m2_rec_yesterday    = yesterday[m2_idx]
+                outrights[id] = []
 
-            id      = f"{m1_rec_today[r.id][-5]}{m1_rec_today[r.id][-1]}-{m2_rec_today[r.id][-5]}{m2_rec_today[r.id][-1]}"
-            settle  = m1_rec_today[r.settle] - m2_rec_today[r.settle]
-            chg     = settle - (m1_rec_yesterday[r.settle] - m2_rec_yesterday[r.settle])
-            text    = f"{id}<br>{date}<br>chg: {chg:0.4f}" 
-
-            if id not in spreads:
-
-                spreads[id] = []
-
-            spreads[id].append(
+            outrights[id].append(
                 (
-                    date,
-                    settle,
-                    text
+                    rec[r.date],
+                    rec[r.settle]
                 )
             )
 
+    # spreads
+
+    if spread_len > 0:
+
+        for i in range(1, len(groups)):
+
+            yesterday   = groups[i - 1]
+            today       = groups[i]
+
+            date = today[0][r.date]
+
+            for j in range(spread_len, min(len(yesterday), len(today), max_terms), spread_len):
+
+                m1_idx = j - spread_len
+                m2_idx = j
+
+                m1_rec_today        = today[m1_idx]
+                m2_rec_today        = today[m2_idx]
+                m1_rec_yesterday    = yesterday[m1_idx]
+                m2_rec_yesterday    = yesterday[m2_idx]
+
+                id      = f"{m1_rec_today[r.id][-5]}{m1_rec_today[r.id][-1]}-{m2_rec_today[r.id][-5]}{m2_rec_today[r.id][-1]}"
+                settle  = m1_rec_today[r.settle] - m2_rec_today[r.settle]
+                chg     = settle - (m1_rec_yesterday[r.settle] - m2_rec_yesterday[r.settle])
+                text    = f"{id}<br>{date}<br>chg: {chg:0.4f}" 
+
+                if id not in spreads:
+
+                    spreads[id] = []
+
+                spreads[id].append(
+                    (
+                        date,
+                        settle,
+                        text
+                    )
+                )
+
     fig = go.Figure()
 
-    for spread, records in spreads.items():
+    if spread_len == 0:
 
-        fig.add_trace(
-            go.Scatter(
-                {
-                    "x": [ rec[0] for rec in records ],
-                    "y": [ rec[1] for rec in records ],
-                    "text": [ rec[2] for rec in records ],
-                    "name": spread
-                }
+        for id, records in outrights.items():
+
+            fig.add_trace(
+                go.Scatter(
+                    {
+                        "x":    [ rec[0] for rec in records ],
+                        "y":    [ rec[1] for rec in records ],     
+                        "name": id
+                    }
+                )    
             )
-        )
+
+    else:
+
+        for id, records in spreads.items():
+
+            fig.add_trace(
+                go.Scatter(
+                    {
+                        "x":    [ rec[0] for rec in records ],
+                        "y":    [ rec[1] for rec in records ],
+                        "text": [ rec[2] for rec in records ],
+                        "name": id
+                    }
+                )
+            )
 
     fig.show()
 
