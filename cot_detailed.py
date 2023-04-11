b3470a70ccc67ab7acd5a08bcc449fc169292e41
Taylor O'Brien <toobrien@amazon.com>
2023-04-10 22:20:17 -0700

    initial pass compelte

--- cot_detailed.py
+++ cot_detailed.py
@@ -18,6 +18,11 @@ def get_stats(cot_recs: List, returns, field, window):
         for rec in cot_recs
     }
 
+    oi_by_date = {
+        rec[cot_rec.date] : rec[cot_rec.oi]
+        for rec in cot_recs
+    }
+
     chg_by_date = {
         cot_recs[i][cot_rec.date] : (cot_recs[i][field] - cot_recs[i - 1][field]) / abs(cot_recs[i - 1][field])
         for i in range(1, len(cot_recs))
@@ -26,7 +31,7 @@ def get_stats(cot_recs: List, returns, field, window):
     ret_by_date = { ret[1] : ret[2] for ret in returns }
 
     zipped = [
-        (date, ret_by_date[date], chg, val_by_date[date])
+        (date, ret_by_date[date], chg, val_by_date[date], oi_by_date[date])
         for date, chg in chg_by_date.items()
         if date in ret_by_date
     ]
@@ -35,19 +40,33 @@ def get_stats(cot_recs: List, returns, field, window):
     rets    = [ rec[1] for rec in zipped ]
     chgs    = [ rec[2] for rec in zipped ]
     vals    = [ rec[3] for rec in zipped ]
+    ois     = [ rec[4] for rec in zipped ]
 
     corrs = [
         correlation(rets[i - window:i], chgs[i - window:i])
         for i in range(window, len(dates))
     ]
 
-    pcts = [
-        (vals[i] - min(vals[i - window:i + 1])) / 
-        (max(vals[i - window:i + 1]) - min(vals[i - window:i + 1]))
-        for i in range(window, len(dates))
-    ]
+    adj_vals = [ vals[i] / ois[i] for i in range(len(dates)) ]
 
-    return ( dates, chgs, corrs, pcts )
+    adj_rng = []
+    max_adj = -1
+    min_adj = 1
+
+    for val in adj_vals:
+
+        max_adj = max(val, max_adj)
+        min_adj = min(val, min_adj)
+
+        try:
+        
+            adj_rng.append((val - min_adj) / (max_adj - min_adj))
+
+        except ZeroDivisionError:
+
+            adj_rng.append(0)
+
+    return ( dates, chgs, corrs, adj_vals, adj_rng )
 
 
 if __name__ == "__main__":
@@ -91,19 +110,22 @@ if __name__ == "__main__":
         ]
     oi      = [ rec[cot_rec.oi] for rec in cot_recs[1:] ]
 
-    comm_dates, comm_chgs, comm_corrs, comm_pcts   = get_stats(cot_recs, returns, cot_rec.comm_net, window)
-    spec_dates, spec_chgs, spec_corrs, spec_pcts   = get_stats(cot_recs, returns, cot_rec.spec_net, window)
+    comm_dates, comm_chgs, comm_corrs, comm_adj, comm_adj_rng = get_stats(cot_recs, returns, cot_rec.comm_net, window)
+    spec_dates, spec_chgs, spec_corrs, spec_adj, spec_adj_rng = get_stats(cot_recs, returns, cot_rec.spec_net, window)
 
     fig = make_subplots(
-            rows            = 4, 
-            cols            = 1,
-            subplot_titles  = ( "price", "net", "corr", "pct" ),
-            specs           = [ 
-                                [ {} ],
-                                [ { "secondary_y": True } ],
-                                [ {} ],
-                                [ {} ]
-                            ]
+            rows                = 5, 
+            cols                = 1,
+            shared_xaxes        = True,
+            subplot_titles      = ( "price", "net", "adj", "adj_rng", "corrs" ),
+            specs               = [ 
+                                    [ {} ],
+                                    [ { "secondary_y": True } ],
+                                    [ {} ],
+                                    [ {} ],
+                                    [ {} ]
+                                ],
+            vertical_spacing    = 0.025
         )
 
     fig.add_trace(
@@ -177,13 +199,64 @@ if __name__ == "__main__":
     fig.add_trace(
         go.Scatter(
             {
+                "x":        comm_dates,
+                "y":        comm_adj,
+                "marker":   { "color": COMM_COLOR },
+                "name":     f"comm_adj"
+            }
+        ),
+        row = 3,
+        col = 1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            {
+                "x":        spec_dates,
+                "y":        spec_adj,
+                "marker":   { "color": SPEC_COLOR },
+                "name":     f"comm_adj"
+            }
+        ),
+        row = 3,
+        col = 1
+    )
+    fig.add_trace(
+        go.Scatter(
+            {
+                "x":        comm_dates,
+                "y":        comm_adj_rng,
+                "marker":   { "color": COMM_COLOR },
+                "name":     f"comm_adj_rng"
+            }
+        ),
+        row = 4,
+        col = 1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            {
+                "x":        spec_dates,
+                "y":        spec_adj_rng,
+                "marker":   { "color": SPEC_COLOR },
+                "name":     f"comm_adj_rng"
+            }
+        ),
+        row = 4,
+        col = 1
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            {
                 "x":        comm_dates[window:],
                 "y":        comm_corrs,
                 "marker":   { "color": COMM_COLOR },
                 "name":     f"comm_corr[{window}]"
             }
         ),
-        row = 3,
+        row = 5,
         col = 1
     )
 
@@ -196,33 +269,7 @@ if __name__ == "__main__":
                 "name":     f"spec_corr[{window}]"
             }
         ),
-        row = 3,
-        col = 1
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            {
-                "x":        comm_dates[window:],
-                "y":        comm_pcts,
-                "marker":   { "color": COMM_COLOR },
-                "name":     f"comm_pct[{window}]"
-            }
-        ),
-        row = 4,
-        col = 1
-    )
-
-    fig.add_trace(
-        go.Scatter(
-            {
-                "x":        spec_dates[window:],
-                "y":        spec_pcts,
-                "marker":   { "color": SPEC_COLOR },
-                "name":     f"comm_pct[{window}]"
-            }
-        ),
-        row = 4,
+        row = 5,
         col = 1
     )
 
