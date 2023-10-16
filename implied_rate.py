b2bbf7e004c18f3acd6dfbe664af1cd481b55130
Taylor O'Brien <toobrien@amazon.com>
2023-10-16 08:54:59 -0700

    .

--- implied_rate.py
+++ implied_rate.py
@@ -1,23 +1,28 @@
-from    data.cat_df import  cat_df
 import  polars      as      pl
-from    sys         import  argv
+from    sys         import  argv, path
+from    time        import  time
+
+path.append("..")
+
+from    data.cat_df import  cat_df
 
 
 # python CME_HOX2023 12 2023-08-01 2024-01-01
 
 
-def report(df: pl.DataFrame):
-
-    pass
+pl.Config.set_tbl_rows(50)
+pl.Config.set_tbl_cols(25)
 
 
 if __name__ == "__main__":
-    
-    symbol  = argv[1]
-    start   = argv[2]
-    end     = argv[3]
 
-    cat_df(
+    t0      = time()
+    symbol  = argv[1]
+    months  = int(argv[2])
+    start   = argv[3]
+    end     = argv[4]
+
+    df = cat_df(
         "futs", 
         symbol, 
         start, 
@@ -28,10 +33,18 @@ if __name__ == "__main__":
         [
             "contract_id",
             "date",
-            "name",
-            "month",
-            "year",
             "settle",
             "dte"
         ]
     )
+
+    contracts = df.filter(df["date"] == df["date"][-1])["contract_id"][0:months]
+
+    df = df.filter(df["contract_id"].is_in(contracts))
+    df = df.pivot(index = "date", columns = "contract_id", values = "settle")
+
+    print(df)
+
+    print(f"elapsed: {time() - t0:0.2f}")
+
+    pass
