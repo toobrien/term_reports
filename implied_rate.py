3f75710fd2c0b26c99a7c6c53972aeda0b7230a0
Taylor O'Brien <toobrien@amazon.com>
2023-10-17 19:40:22 -0700

    .

--- implied_rate.py
+++ implied_rate.py
@@ -1,12 +1,12 @@
-import  numpy       as      np
-import  pandas      as      pd
-import  polars      as      pl
-from    sys         import  argv, path
-from    time        import  time
+import  numpy                   as      np
+import  plotly.graph_objects    as  go
+import  polars                  as      pl
+from    sys                     import  argv, path
+from    time                    import  time
 
 path.append("..")
 
-from    data.cat_df import  cat_df
+from    data.cat_df             import  cat_df
 
 
 # python CME_HOX2023 12 2023-08-01 2024-01-01
@@ -43,19 +43,40 @@ if __name__ == "__main__":
     contracts   = df.filter(df["date"] == df["date"][-1])["contract_id"]
     contracts   = list(contracts[0:months])
     df          = df.filter(df["contract_id"].is_in(contracts))
-    df          = df.to_pandas()
-
-    df_price    = df.pivot_table(index = "date", columns = "contract_id", values = "settle")[contracts]
-    df_dte      = df.pivot_table(index = "date", columns = "contract_id", values = "dte")[contracts]
     
-    m1_price    = pd.concat([ df_price.iloc[:, 0] ] * (len(contracts) - 1), axis = 1, ignore_index = True)
-    dur         = (df_dte.subtract(df_dte.iloc[:, 0], axis = 0).iloc[0].T)[1:] / 365
-    df_price    = df_price.iloc[:, 1:]
+    settles     = df.pivot(index = "date", columns = "contract_id", values = "settle", aggregate_function = "first")
+    dtes        = df.pivot(index = "date", columns = "contract_id", values = "dte", aggregate_function = "first")
+    
+    dates       = settles.select("date")
+    m1_settles  = settles[contracts[0]]
+    m1_dtes     = dtes[contracts[0]]
 
-    x           = df_price.values
-    y           = m1_price.values
-    t           = dur.values
+    settles     = settles.drop( ["date", contracts[0] ])
+    dtes        = dtes.drop([ "date", contracts[0] ])
 
-    df_rate     = np.log(x / y) / t
-   
+    dates       = np.array(dates)
+    m1_settles  = np.tile(np.array(m1_settles), ( settles.shape[1], 1 )).T
+    m1_dtes     = np.tile(np.array(m1_dtes), ( dtes.shape[1], 1 )).T
+    settles     = settles.to_numpy()
+    dtes        = dtes.to_numpy()
+
+    #print(dates)
+    #print(m1_settles)
+    #print(m1_dtes)
+    #print(settles)
+    #print(dtes)
+
+    logs    = np.log(settles / m1_settles)
+    dtes    = dtes - m1_dtes
+
+    rates   = logs / (dtes / 365)
+
+    print(logs)
+    print(dtes)
+    print(rates)
+
+    fig = go.Figure(data = go.Surface(z = rates))
+
+    fig.show()
+
     pass
